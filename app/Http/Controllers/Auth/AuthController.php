d75192d8f8b4b8a7d9fcfea1e615895cac957367
Kenji Goto <realbuzzinc@gmail.com>
2022-09-29 07:12:53 +0900

    hashpassword debug

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginFormRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -24,6 +25,8 @@ class AuthController extends Controller
 
     public function login(LoginFormRequest $request) 
     {
+##    dd($request['password']);  # 2022Ken510
+##    dd(Hash::make($request['password']));
       $credentials = $request->only('email, password');
 
       if (Auth::attempt($credentials)){
